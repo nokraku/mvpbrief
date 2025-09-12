3b8f6b54d50fb9555b3585d43f30b954fbdf55d4
nokraku <nokraku@hotmail.com>
2025-09-12 17:09:57 +0100

    Layout syntax for Google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter } from 'next/font/google';
 import Script from 'next/script';
 import './globals.css';
 
-
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
@@ -18,21 +17,18 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-      import Script from 'next/script'
-
-// Add these script tags in your layout
-<Script src="https://www.googletagmanager.com/gtag/js?id=G_VJ3LKY7EK8" />
-<Script id="google-analytics">
-  {`
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', 'G_VJ3LKY7EK8');
-  `}
-</Script>
-      </head>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G_VJ3LKYEK8" />
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G_VJ3LKYEK8');
+          `}
+        </Script>
+        {children}
+      </body>
     </html>
   );
 }
